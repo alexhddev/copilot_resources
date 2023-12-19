78348af423d2cdc29ce3494be74e50c36eab8893
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-12-19 10:04:35 +0200

    Refactor main method documentation

--- Java/my-app/src/main/java/com/barosanu/App.java
+++ Java/my-app/src/main/java/com/barosanu/App.java
@@ -1,11 +1,14 @@
 package com.barosanu;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
+    /**
+     * The main method is the entry point of the Java application.
+     * It is called when the program starts and is responsible for executing the program logic.
+     *
+     * @param args the command line arguments passed to the program
+     */
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );

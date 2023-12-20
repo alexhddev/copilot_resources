a881807e8c10af0850ef4e84209758c03efa9eb7
alexhddev <21276888+alexhddev@users.noreply.github.com>
2023-12-20 13:48:54 +0200

    Add hashCode() and equals() methods to Employee class

--- Java/my-app/src/main/java/com/barosanu/Employee.java
+++ Java/my-app/src/main/java/com/barosanu/Employee.java
@@ -36,6 +36,39 @@ public class Employee {
         this.salary = salary;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + age;
+        result = prime * result + salary;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Employee other = (Employee) obj;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (age != other.age)
+            return false;
+        if (salary != other.salary)
+            return false;
+        return true;
+    }
+
+    
+
 
     
 }

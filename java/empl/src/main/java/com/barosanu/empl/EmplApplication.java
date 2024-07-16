e2694ed171f213834631f2cddfcd91b66b3dd77e
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-07-16 11:21:07 +0300

    spring project init

---
+++ java/empl/src/main/java/com/barosanu/empl/EmplApplication.java
@@ -0,0 +1,13 @@
+package com.barosanu.empl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmplApplication.class, args);
+	}
+
+}

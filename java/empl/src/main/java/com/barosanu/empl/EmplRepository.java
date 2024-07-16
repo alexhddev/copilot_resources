e79673dfdb3f2d997a02a0d97d2aa2c265d4ce47
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-07-16 16:04:44 +0300

    Add EmplController, EmplRepository, and EmplService classes

---
+++ java/empl/src/main/java/com/barosanu/empl/EmplRepository.java
@@ -0,0 +1,11 @@
+package com.barosanu.empl;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.barosanu.empl.model.Employee;
+
+public interface EmplRepository extends JpaRepository<Employee, Long> {
+
+    List<Employee> findByPosition(String position);
+}
---
+++ java/empl/src/main/java/com/barosanu/empl/EmplService.java
@@ -0,0 +1,42 @@
+package com.barosanu.empl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.barosanu.empl.model.Employee;
+
+@Service
+public class EmplService {
+    @Autowired
+    private EmplRepository employeeRepository;
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee getEmployeeById(Long id) {
+        return employeeRepository.findById(id).get();
+    }
+
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    public String updateEmployeesPosition(String id, String position){
+        Employee employee = employeeRepository.findById(Long.parseLong(id)).get();
+        employee.setPosition(position);
+        employeeRepository.save(employee);
+        return "Employee position updated";
+    }
+
+    public String deleteEmployeeById(Long id) {
+        employeeRepository.deleteById(id);
+        return "Employee deleted";
+    }
+
+    public List<Employee> getEmployeesByPosition(String position){
+        return employeeRepository.findByPosition(position);
+    }
+}

5aef9f6de731ae4bc6f22b15a0c78b4b3739316d
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-07-25 09:23:41 +0300

    project setup

---
+++ React/empl-front/src/components/AddEmple.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the AddEmpl component!</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ React/empl-front/src/components/ListEmpl.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the ListEmpl component!</h1>
+        </div>
+    );
+};
+
+export default Home;

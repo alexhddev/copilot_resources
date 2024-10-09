5ca0626388604cd849c0890f19ece467052ef54d
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-10-09 11:03:44 +0300

    The completitions panel

---
+++ starter/data.ts
@@ -0,0 +1,23 @@
+export interface Employee {
+    name: string;
+    age: number;
+    salary: number;
+}
+
+const employees: Employee[] = [
+    { name: 'John', age: 25, salary: 2000 },
+    { name: 'Linda', age: 30, salary: 2500 },
+    { name: 'Alice', age: 35, salary: 3000 },
+    { name: 'Bob', age: 40, salary: 3500 },
+    { name: 'Peter', age: 45, salary: 4000 },
+    { name: 'Karen', age: 50, salary: 4500 },
+    { name: 'Mary', age: 55, salary: 5000 },
+    { name: 'George', age: 60, salary: 5500 },
+    { name: 'Lisa', age: 65, salary: 6000 },
+    { name: 'John', age: 70, salary: 6500 },
+];
+
+function getMinMaxSalary(){
+
+}
+
